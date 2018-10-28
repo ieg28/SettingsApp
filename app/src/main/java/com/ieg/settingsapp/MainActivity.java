c32c615fda6f10d470842fbda4491a30dc58d1ca
Ibrahim Ethem Gursoy <iegursoy.2888@gmail.com>
2018-10-28 12:31:16 +0300

    first commit

---
+++ app/src/main/java/com/ieg/settingsapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.ieg.settingsapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    //mTextMessage.setText(R.string.title_notifications);
+                    startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mTextMessage != null) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+            String txt = String.format("auto sync: %s \nuser name: %s\nsync time %s",
+                    preferences.getBoolean(SettingsActivity.key_auto_sync, false),
+                    preferences.getString(SettingsActivity.key_user_name, "not set"),
+                    preferences.getString(SettingsActivity.key_sync_time, "not set"));
+
+            mTextMessage.setText(txt);
+        }
+    }
+}
